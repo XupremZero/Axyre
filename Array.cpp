a025202d823dff649eeb795b2a071be9b1574dc3
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-09-11 07:04:03 +0200

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include "Array.h"
+
+template<class T, size_t Size>
+Axyre::Array<T, Size>::Array(const T[] values)
+{
+	Axyre::Array<T, Size>::Values = values;
+}
+
+template<class T, size_t Size>
+Axyre::Array<T, size_t Size>::GetElement(const UInt ID)
+{
+	if (ID <= Size)
+		return Axyre::Array<T, Size>::Values[ID];
+}
