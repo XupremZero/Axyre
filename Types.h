2848389a336d72fc069f1c4361ee3ba61124bbd5
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-09-11 06:16:56 +0200

    Create Types.h

---
+++ Types.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+#define IL inline
+#define GetSize sizeof
+
+namespace Axyre 
+{
+	typedef void Void;
+	typedef void* Delegate;
+	typedef bool Bit;
+	typedef float Single;
+	typedef double Double;
+	typedef std::uint8_t UByte;
+	typedef std::int8_t Byte;
+	typedef std::uint16_t UShort;
+	typedef std::int16_t Short;
+	typedef std::uint32_t UInt;
+	typedef std::int32_t Int;
+	typedef std::uint64_t ULong;
+	typedef std::int64_t Long;
+}
