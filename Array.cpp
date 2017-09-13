066ef54cd37bdc1af1e8c33a43cfe5e77f0509b7
Samuel Alonso Rodríguez <somethingghanging@gmail.com>
2017-09-13 06:09:37 +0200

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,14 +1,42 @@
 #include "Array.h"
 
-template<class T, size_t Size>
+template<class T, Axyre::UInt Size>
 Axyre::Array<T, Size>::Array(const T[] values)
 {
 	Axyre::Array<T, Size>::Values = values;
 }
 
-template<class T, size_t Size>
-Axyre::Array<T, Size>::GetElement(const UInt ID)
+template<class T, Axyre::UInt Size>
+T Axyre::Array<T, Size>::Back()
+{
+	return Axyre::Array<T, Size>::Values[Size - 1];
+}
+
+template<class T, Axyre::UInt Size>
+T Axyre::Array<T, Size>::Front ()
+{
+	return Axyre::Array<T, Size>::Values[0];
+}
+
+template<class T, Axyre::UInt Size>
+void Axyre::Array<T, Size>::Reset()
+{
+	for (UInt aux = 0; aux < Size; aux++)
+		Axyre::Array<T, Size>::Values[aux] = 0;
+}
+
+template<class T, Axyre::UInt Size>
+T Axyre::Array<T, Size>::GetElement(const UInt ID)
 {
 	if (ID <= Size)
 		return Axyre::Array<T, Size>::Values[ID];
+	else
+		return 0;
+}
+
+template<class T, Axyre::UInt Size>
+void Axyre::Array<T, Size>::SetElement(const UInt ID, const T NewValue)
+{
+	if(ID <= Size)
+		Axyre::Array<T, Size>::Values[ID] = NewValue;
 }
